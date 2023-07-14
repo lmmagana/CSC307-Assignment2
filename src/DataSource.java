3b6dc4564fb314ce1c2cd31ec5a290afe419f776
Luis Magana <lmmagana@calpoly.edu>
2023-07-14 13:30:26 -0700

    Lab7 - Update Dot and DataSource

--- src/DataSource.java
+++ src/DataSource.java
@@ -1,2 +1,20 @@
-public class DataSource {
+import java.util.ArrayList;
+import java.util.Observable;
+
+public class DataSource extends Observable {
+
+    private static ArrayList<Dot> _instance;
+
+    protected DataSource(){}
+
+    public static ArrayList<Dot> getData(){
+        if(_instance.size() == 0){
+            _instance = new ArrayList<Dot>();
+        }
+        return _instance;
+    }
+
+    public void addData(Dot d){
+        _instance.add(d);
+    }
 }
--- src/Dot.java
+++ src/Dot.java
@@ -1,2 +1,37 @@
+import java.awt.*;
+
 public class Dot {
+
+    private int positionX, positionY;
+    private Color color;
+
+    public Dot(int positionX, int positionY, Color color){
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.color = color;
+    }
+
+    public int getX() {
+        return positionX;
+    }
+
+    public void setX(int positionX) {
+        this.positionX = positionX;
+    }
+
+    public int getY() {
+        return positionY;
+    }
+
+    public void setY(int positionY) {
+        this.positionY = positionY;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
 }
